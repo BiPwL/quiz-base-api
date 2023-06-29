932e92a6feec5b20a56cd3c148cb9f71c8fd4893
BiPwL <asmaev.daniil@mail.ru>
2023-06-30 00:12:43 +0300

    Update init schema add cascade deleting for all FOREIGN KEY values

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,33 +1,39 @@
-CREATE TABLE "answers" (
-                          "id" BIGSERIAL PRIMARY KEY NOT NULL,
-                          "question_id" bigint NOT NULL,
-                          "text" varchar NOT NULL,
-                          "is_correct" bool NOT NULL DEFAULT False
+CREATE TABLE "answers"
+(
+    "id"          BIGSERIAL PRIMARY KEY NOT NULL,
+    "question_id" bigint                NOT NULL,
+    "text"        varchar               NOT NULL,
+    "is_correct"  bool                  NOT NULL DEFAULT False
 );
 
-CREATE TABLE "questions" (
-                            "id" BIGSERIAL PRIMARY KEY NOT NULL,
-                            "text" varchar NOT NULL,
-                            "hint" varchar NOT NULL,
-                            "category" varchar NOT NULL
+CREATE TABLE "questions"
+(
+    "id"       BIGSERIAL PRIMARY KEY NOT NULL,
+    "text"     varchar               NOT NULL,
+    "hint"     varchar               NOT NULL,
+    "category" varchar               NOT NULL
 );
 
-CREATE TABLE "categories" (
-                            "name" varchar UNIQUE NOT NULL,
-                            "key" varchar UNIQUE NOT NULL
+CREATE TABLE "categories"
+(
+    "key"  varchar UNIQUE NOT NULL,
+    "name" varchar UNIQUE NOT NULL
 );
 
-CREATE TABLE "answered_questions" (
-                                     "user_id" bigint NOT NULL,
-                                     "question_id" bigint NOT NULL,
-                                     "answered_at" TIMESTAMP NOT NULL DEFAULT (now())
+CREATE TABLE "answered_questions"
+(
+    "id"          BIGSERIAL PRIMARY KEY NOT NULL,
+    "user_id"     bigint    NOT NULL,
+    "question_id" bigint    NOT NULL,
+    "answered_at" TIMESTAMP NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE "users" (
-                        "id" BIGSERIAL PRIMARY KEY NOT NULL,
-                        "email" varchar UNIQUE NOT NULL,
-                        "password" varchar NOT NULL,
-                        "created_at" TIMESTAMP NOT NULL DEFAULT (now())
+CREATE TABLE "users"
+(
+    "id"         BIGSERIAL PRIMARY KEY NOT NULL,
+    "email"      varchar UNIQUE        NOT NULL,
+    "password"   varchar               NOT NULL,
+    "created_at" TIMESTAMP             NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "answers" ("question_id");
@@ -48,14 +54,20 @@ CREATE INDEX ON "answered_questions" ("user_id", "question_id");
 
 CREATE INDEX ON "users" ("id");
 
-COMMENT ON COLUMN "categories"."name" IS 'name for user';
+COMMENT
+ON COLUMN "categories"."name" IS 'name for user';
 
-COMMENT ON COLUMN "categories"."key" IS 'name for dev';
+COMMENT
+ON COLUMN "categories"."key" IS 'name for dev';
 
-ALTER TABLE "answers" ADD FOREIGN KEY ("question_id") REFERENCES "questions" ("id");
+ALTER TABLE "answers"
+    ADD FOREIGN KEY ("question_id") REFERENCES "questions" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "questions" ADD FOREIGN KEY ("category") REFERENCES "categories" ("key");
+ALTER TABLE "questions"
+    ADD FOREIGN KEY ("category") REFERENCES "categories" ("key") ON DELETE CASCADE;
 
-ALTER TABLE "answered_questions" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "answered_questions"
+    ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "answered_questions" ADD FOREIGN KEY ("question_id") REFERENCES "questions" ("id");
+ALTER TABLE "answered_questions"
+    ADD FOREIGN KEY ("question_id") REFERENCES "questions" ("id") ON DELETE CASCADE;
