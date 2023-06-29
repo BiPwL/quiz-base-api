e83333c5ac54052f663d55e83b04c224fa22f407
BiPwL <asmaev.daniil@mail.ru>
2023-06-29 22:55:22 +0300

    Сreated a pattern for generating CRUD commands for users

---
+++ db/queries/users.sql
@@ -0,0 +1,28 @@
+-- name: CreateUser :one
+INSERT INTO "users" ("email",
+                     "password")
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: GetUser :one
+SELECT *
+FROM "users"
+WHERE "id" = $1
+LIMIT 1;
+
+-- name: ListUsers :many
+SELECT *
+FROM "users"
+ORDER BY "id"
+LIMIT $1 OFFSET $2;
+
+-- name: UpdateUser :one
+UPDATE "users"
+SET "password" = $2
+WHERE "id" = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE
+FROM "users"
+WHERE "id" = $1;
