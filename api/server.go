06a4ccfb95685f5e46c802c92e3a8bc9f01cf5f8
BiPwL <asmaev.daniil@mail.ru>
2023-07-08 15:14:32 +0300

    Create file for working with server

---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+
+	db "github.com/BiPwL/quiz-base-api/db/sqlc"
+)
+
+// Server serves HTTP requests
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("users", server.createUser)
+	router.GET("users/:id", server.getUser)
+	router.GET("users", server.listUsers)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
