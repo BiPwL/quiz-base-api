019f973172a2c2e3a8017f7a5975d0fcb647ada2
BiPwL <asmaev.daniil@mail.ru>
2023-07-21 12:30:49 +0300

    feature(category-api): create ListCategoryQuestions handler

--- api/category.go
+++ api/category.go
@@ -136,3 +136,31 @@ func (server *Server) updateCategory(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, category)
 }
+
+type listCategoryQuestions struct {
+	Category string `form:"category" binding:"required"`
+	PageID   int32  `form:"page_id" binding:"required,min=1"`
+	PageSize int32  `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) ListCategoryQuestions(ctx *gin.Context) {
+	var req listCategoryQuestions
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListCategoryQuestionsParams{
+		Category: req.Category,
+		Limit:    req.PageSize,
+		Offset:   (req.PageID - 1) * req.PageSize,
+	}
+
+	questions, err := server.store.ListCategoryQuestions(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, questions)
+}
