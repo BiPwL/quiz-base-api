5ecec675619bba898814606875221919099b5663
BiPwL <asmaev.daniil@mail.ru>
2023-07-22 17:31:47 +0300

    feature(db-operations-sql-go): create CleanTable functions

--- db/sqlc/db_operations.go
+++ db/sqlc/db_operations.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 const tableExists = `-- name: CleanTable :one
@@ -14,4 +15,14 @@ func (q *Queries) TableExists(ctx context.Context, tableName string) (bool, erro
 	var isExists bool
 	err := row.Scan(&isExists)
 	return isExists, err
+}
+
+const cleanTable = `-- name: CleanTable :exec
+DELETE FROM %s;
+ALTER SEQUENCE %s_id_seq RESTART WITH 1;
+`
+
+func (q *Queries) CleanTable(ctx context.Context, tableName string) error {
+	_, err := q.db.ExecContext(ctx, fmt.Sprintf(cleanTable, tableName, tableName))
+	return err
 }
