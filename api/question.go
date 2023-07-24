335132e53b049c70da92086255a56dbdea821246
BiPwL <asmaev.daniil@mail.ru>
2023-07-24 15:53:26 +0300

    feature(question-qpi): create listQuestionAnswers handler

--- api/question.go
+++ api/question.go
@@ -146,3 +146,31 @@ func (server *Server) updateQuestion(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, question)
 }
+
+type listQuestionAnswersRequest struct {
+	QuestionID int64 `form:"question_id" binding:"required"`
+	PageID     int32 `form:"page_id" binding:"required,min=1"`
+	PageSize   int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listQuestionAnswers(ctx *gin.Context) {
+	var req listQuestionAnswersRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListQuestionAnswersParams{
+		QuestionID: req.QuestionID,
+		Limit:      req.PageSize,
+		Offset:     (req.PageID - 1) * req.PageSize,
+	}
+
+	answers, err := server.store.ListQuestionAnswers(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, answers)
+}
