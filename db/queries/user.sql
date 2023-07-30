6a92c31b88ddf2a869f517d0c0630aeff0328fe3
BiPwL <asmaev.daniil@mail.ru>
2023-07-31 02:24:00 +0300

    feature(user-sql): create GetUserAnsweredQuestionsCount sql function

--- db/queries/user.sql
+++ db/queries/user.sql
@@ -38,3 +38,10 @@ FROM "answered_questions" AS aq
 WHERE aq.user_id = sqlc.arg(user_id)
   AND (sqlc.arg(category)::TEXT = '' OR q.category = sqlc.arg(category)::TEXT)
 LIMIT $1 OFFSET $2;
+
+-- name: GetUserAnsweredQuestionsCount :one
+SELECT COUNT(*)
+FROM "answered_questions" AS aq
+         JOIN "questions" AS q ON aq.question_id = q.id
+WHERE aq.user_id = sqlc.arg(user_id)
+  AND (sqlc.arg(category)::TEXT = '' OR q.category = sqlc.arg(category)::TEXT);
