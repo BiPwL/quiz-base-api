c93f4a91ad6f8f5b00a92c2aa7ae15088a66325e
BiPwL <asmaev.daniil@mail.ru>
2023-08-02 10:23:41 +0300

    refactor(store-go): use Store interface

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -7,7 +7,6 @@ import (
 )
 
 // Store provides all functions to execute db queries and transactions
-type Store struct {
 type Store interface {
 	Querier
 	TableExists(ctx context.Context, tableName string) (bool, error)
@@ -15,20 +14,22 @@ type Store interface {
 	CleanTables(ctx context.Context, tablesNames []string) []error
 }
 
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
 // NewStore create a new Store
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a functions within a database transactions
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
