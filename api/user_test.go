bea51992b4d59a65af46c8c4c6dcb96d0979852f
BiPwL <asmaev.daniil@mail.ru>
2023-08-02 12:53:59 +0300

    test(user-api): create requireBodyMatchUser function

--- api/user_test.go
+++ api/user_test.go
@@ -1,6 +1,13 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
 	db "github.com/BiPwL/quiz-base-api/db/sqlc"
 	"github.com/BiPwL/quiz-base-api/util"
 )
@@ -11,4 +18,14 @@ func randomUser() db.User {
 		Email:          util.RandomEmail(),
 		HashedPassword: util.RandomPasswordStr(10),
 	}
+}
+
+func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user db.User) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotUser db.User
+	err = json.Unmarshal(data, &gotUser)
+	require.NoError(t, err)
+	require.Equal(t, user, gotUser)
 }
